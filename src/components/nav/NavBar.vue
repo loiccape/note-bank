8d7354937c0073b7947e325732ca372ecbcbe212
Loïc Cape <loiccape.pro@outlook.fr>
2024-11-26 11:31:03 +0100

    navbar responsiv md

--- src/components/nav/NavBar.vue
+++ src/components/nav/NavBar.vue
@@ -1,14 +1,19 @@
 <template>
-    <nav class="bg-grey900 fixed bottom-0 w-full h-12">
+    <nav class="bg-grey900 fixed bottom-0 w-full h-12 md:h-20">
         <img src="../../assets/images/logo-large.svg" alt="logo finance" class="hidden lg:block" />
 
         <ul class="w-full h-full flex justify-evenly items-center text-grey300">
             <!-- Boucle sur les icônes pour les rendre dynamiques si besoin -->
-            <NavIcon :icon="DocumentTextIcon" :index="1" :isActive="activeIndex === 1" @click="handleClick(1)" />
-            <NavIcon :icon="HomeIcon" :index="2" :isActive="activeIndex === 2" @click="handleClick(2)" />
-            <NavIcon :icon="AdjustmentsVerticalIcon" :index="3" :isActive="activeIndex === 3" @click="handleClick(3)" />
-            <NavIcon :icon="ChartPieIcon" :index="4" :isActive="activeIndex === 4" @click="handleClick(4)" />
-            <NavIcon :icon="WalletIcon" :index="5" :isActive="activeIndex === 5" @click="handleClick(5)" />
+            <NavIcon :icon="DocumentTextIcon" title="Overview" :index="1" :isActive="activeIndex === 1"
+                @click="handleClick(1)" />
+            <NavIcon :icon="HomeIcon" title="Transactions" :index="2" :isActive="activeIndex === 2"
+                @click="handleClick(2)" />
+            <NavIcon :icon="AdjustmentsVerticalIcon" title="Budgets" :index="3" :isActive="activeIndex === 3"
+                @click="handleClick(3)" />
+            <NavIcon :icon="ChartPieIcon" title="Pots" :index="4" :isActive="activeIndex === 4"
+                @click="handleClick(4)" />
+            <NavIcon :icon="WalletIcon" title="Recurring bills" :index="5" :isActive="activeIndex === 5"
+                @click="handleClick(5)" />
         </ul>
     </nav>
 </template>
--- src/components/nav/NavIcon.vue
+++ src/components/nav/NavIcon.vue
@@ -1,9 +1,10 @@
 <template>
     <li @click="$emit('click')" :class="[
-        'cursor-pointer flex flex-col items-center justify-center w-12 h-12',
+        'cursor-pointer flex flex-col items-center justify-center h-full w-12 md:w-32 mt-2 rounded-t-lg',
         isActive ? 'bg-white text-grey900' : 'bg-grey900 text-grey300',
     ]">
-        <component :is="icon" class="h-6 w-6" />
+        <component :is="icon" class="w-6 md:w-10" />
+        <p class="hidden md:block font-bold">{{ title }}</p>
     </li>
 </template>
 
@@ -14,6 +15,10 @@ defineProps({
     icon: {
         type: Object,
         required: true,
+    },
+    title: {
+        type: String,
+        required: true
     },
     isActive: {
         type: Boolean,
