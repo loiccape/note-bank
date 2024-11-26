5ac1babd0f8c3ec41aa5b571d063e4fa3dec5ada
Loïc Cape <loiccape.pro@outlook.fr>
2024-11-26 12:13:00 +0100

    home top containers

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,31 @@
 <template>
     <div class="flex">
         <!-- Navbar -->
-        <NavBar class="fixed lg:relative lg:w-16 lg:h-screen" />
+        <NavBar />
 
-        <!-- Contenu principal -->
-        <main class="flex-1 lg:ml-16">
-            <section>
-                <p>iyuhgiyugh</p>
+
+        <main class="p-4 w-screen md:p-8">
+            <h2 class="text-3xl font-bold">Overview</h2>
+
+
+            <section class="flex flex-col w-full gap-2 mt-8 md:flex-row md:gap-5 md:h-32">
+                <div
+                    class="h-24 rounded-lg bg-grey900 text-white p-4 flex flex-col justify-between md:w-1/3 md:p-6 md:h-full">
+                    <p class="md:text-2xl">Title</p>
+                    <p class="text-3xl font-bold">$5,678.45</p>
+                </div>
+                <div class="h-24 rounded-lg bg-white p-4 flex flex-col justify-between md:w-1/3 md:p-6 md:h-full">
+                    <p class="md:text-2xl">Title</p>
+                    <p class="text-3xl font-bold">$5,678.45</p>
+                </div>
+                <div class="h-24 rounded-lg bg-white p-4 flex flex-col justify-between md:w-1/3 md:p-6 md:h-full">
+                    <p class="md:text-2xl">Title</p>
+                    <p class="text-3xl font-bold">$5,678.45</p>
+                </div>
             </section>
+
+            <!-- Pots section -->
+
         </main>
     </div>
 </template>
